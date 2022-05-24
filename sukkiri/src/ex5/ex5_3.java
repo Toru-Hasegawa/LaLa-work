c547cd38bbee0e7e9bea8c0fb74c62b2ea98521d
Toru-Hasegawa <toruhasegawa0911@gmail.com>
2022-05-24 22:06:07 +0900

    20220524homework

--- sukkiri/src/ex5/ex5_3.java
+++ sukkiri/src/ex5/ex5_3.java
@@ -3,12 +3,17 @@ package ex5;
 public class ex5_3 {
 
 	public static void main(String[] args) {
-		String title = "挨拶";
 		String address = "abc123@gmail.com";
 		String text = "おはようございます！";
-		email(title, address, text);
+		email(address, text);
 	}
 
+	public static void email(String address, String text) {
+		System.out.println(address + "に以下のメールを送信しました");
+		System.out.println("件名:無題");
+		System.out.println("本文:" + text);
+	}
+	
 	public static void email(String title, String address, String text) {
 		System.out.println(address + "に以下のメールを送信しました");
 		System.out.println("件名:" + title);
